dd707ad37f96eb02525d9f27699feb60d023d202
Vitalflea <sccarls1@yahoo.com>
2024-09-05 16:36:12 -0700

    update

--- VH2/src/hooks/other/sub_180707F00.cpp
+++ VH2/src/hooks/other/sub_180707F00.cpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "hooks/other/sub_180707F00.hpp"
-
+#include "tools/Tools.hpp"
 sub_180707F00::sub_180707F00(void* address)
 	: m_address{ address } {
 
@@ -25,11 +25,29 @@ sub_180707F00::~sub_180707F00() {
 	logger::Info("sub_180707F00 Unhooked: 0x%llx", this->m_address);
 }
 
+class test {
+	public:
+	char pad[0x153C];
+	std::uint8_t count;
+};
+
 std::int64_t* sub_180707F00::hook(int a1, int a2, __int64 a3, int a4, __int64 a5, float a6, __int64 a7, char a8, float a9, unsigned int a10) {
 
 	AtomicHook atomicHook(&m_flag);
 
-	logger::Info("a1: %d a2: %d a3: %ll a4: %d a5: %ll a7: %f a8: %c a9: %f a10: %d", a1, a2, a3, a4, a5, a6, a7, a8, a9, a10);
+	static auto obj = tools::GetAddress<test*>("halo2.dll", 0x15FD020, false);
+	if (obj == nullptr) {
+	
+		obj = tools::GetAddress<test*>("halo2.dll", 0x15FFB30, false);
+	}
+
+	logger::Info("%c", obj->count); 
+	// logger::Info("a1: %d a2: %d a3: %ll a4: %d a5: %ll a7: %f a8: %c a9: %f a10: %d", a1, a2, a3, a4, a5, a6, a7, a8, a9, a10);
+
+	if (obj->count >= 256) {
+
+		obj->count = 255;
+	}
 
 	return m_original(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10);
 }
